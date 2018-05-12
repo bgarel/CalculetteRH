6b62af8ac532889f3faf6a40201e925c089ece2e
Benjamin GAREL <bgarel@hotmail.com>
2018-05-12 11:30:32 +0200

    Remise à zéro

--- src/app/_references.ts
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path='./GestionPersonnel/Anciennete/AncienneteController.ts'/>
-/// <reference path='./GestionPersonnel/ConversionMinCent/ConversionMinCentController.ts'/>
-/// <reference path='./GestionPersonnel/HeuresCompl/HeuresComplController.ts'/>
-/// <reference path='./GestionPersonnel/HeuresSup/HeuresSupController.ts'/>
-/// <reference path='./Paie/IjssSalaireMensualise/IjssSalaireMenController.ts'/>
-/// <reference path='./Paie/IndemLic/IndemLicController.ts'/>
-
