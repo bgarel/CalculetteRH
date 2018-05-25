538a102bf235cfe0f8ac5c7c16699858370c4ad2
Benjamin GAREL <benjamin.garel@axeria-iard.fr>
2018-05-25 17:13:06 +0200

    Ajout redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from './app-navbar';
 import Anciennete from './GestionPersonnel/anciennete';
 import 'moment/locale/fr';
+import { connect } from 'react-redux';
+import {simpleAction} from './Actions/simpleAction';
+
 
 class App extends Component {
+
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
   render() {
     return (
       <main>
         <AppNavBar/>                
         <Anciennete/>
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+      </pre>
       </main>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+ })
+
+ const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
